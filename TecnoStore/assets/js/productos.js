106e8f9a97c2f51c49c067e3373955bbd7be5937
Matías Díaz <mati98ld@gmail.com>
2024-09-19 13:56:20 -0300

    Se agregaron notebooks a lista de productos y pages

--- TecnoStore/assets/js/productos.js
+++ TecnoStore/assets/js/productos.js
@@ -41,4 +41,50 @@ const productos = [
     descripcion:
       "Tablet procesador Octa-Core. Ponla de pie, cuelga e inclina tu tablet con un soporte de acero inoxidable con una impresionante calidad de imagen 2K a través de Dolby Vision. Audio cinematográfico en movimiento con Dolby Atmos® con cuatro altavoces JBL. Obtén una mayor productividad con doble pantalla.Y el Lenovo Precision Pen 2 opcional (no incluido) es un regalo del cielo para los creadores, ya que te ofrece la destreza y la libertad para dibujar diseños directamente en la pantalla, tal y como lo harías en un bloc de dibujo.",
   },
+  {
+    nombre:
+      'Notebook Samsung 15.6" Intel Core i5 10 Núcleos 512GB 8GB Galaxy Book3 Silver',
+    marca: "Samsung",
+    codigo: "NP750XFG-KB2A",
+    precio: 1299999,
+    destacado: true,
+    imagenes: [
+      "https://images.fravega.com/f300/6a74c2c706cd166957e58b33d6c1e311.png.webp",
+      "https://images.fravega.com/f300/e232c362963ee10936f5355e30aac34e.png.webp",
+      "https://images.fravega.com/f300/44992f6ace94937f111b23eddea23601.png.webp",
+      "https://images.fravega.com/f300/091572880daaaa7a165cff02d0c6d5bd.png.webp",
+    ],
+    descripcion:
+      "La notebook Samsung Cuenta con un diseño delgado, un peso de menos de 1,6 kg y una pantalla de 15,6 pulgadas y resolución FHD (1920 x 1080). Su memoria de 8 GB LPDDR, 512 GB de almacenamiento SSD y el procesador Intel Core i5 te brindarán una gran eficiencia y velocidad. Cuenta con puertos 2 USB tipo C y 2 USB 3.2, 1 HDMI y una  batería de larga duración de 54 Wh, que ofrece hasta 12 horas de autonomía, podrás usar la notebook durante todo el día sin tener que preocuparte.",
+  },
+  {
+    nombre: 'Notebook HP Core Ultra 5 14" 8GB 512GB SSD',
+    marca: "HP",
+    codigo: "14-EP1002LA",
+    precio: 1399999,
+    destacado: true,
+    imagenes: [
+      "https://images.fravega.com/f300/e4ba035aec6e4ac51263711ec7e7c13f.jpg.webp",
+      "https://images.fravega.com/f300/c3c0966af7318d899dd8871b0c814f51.jpg.webp",
+      "https://images.fravega.com/f300/706043b4472bd406046b6c533f48cb9d.jpg.webp",
+      "https://images.fravega.com/f300/eefae7f06d59d5125e269c2989aac538.jpg.webp",
+    ],
+    descripcion:
+      "Combinando su procesador Intel Core Ultra 5 con la memoria RAM de 8GB y el almacenamiento SSD de 512GB, la HP 14-EP1002LA ofrece un gran desempeño. Con una pantalla de 14 pulgadas y su resolución FHD, podrás usar la notebook HP para trabajar o estudiar con comodidad. Con su batería de 3 celdas y 41 Wh podrás llevar tu Notebook HP 14-EP1002LA con vos a todos lados y mantener tu productividad al máximo ya que la duración de esta batería es de hasta 8 horas y 45 minutos. Viene con el sistema operativo Windows 11",
+  },
+  {
+    nombre: "Notebook Acer 15,6” AMD Ryzen 5 8GB 256GB SSD Aspire 3",
+    marca: "Acer",
+    codigo: "A315-24P-R4JA",
+    precio: 999999,
+    destacado: false,
+    imagenes: [
+      "https://images.fravega.com/f300/40a0dac3acabb2f5cfa724ba669cbe7d.jpg.webp",
+      "https://images.fravega.com/f300/f922626c8f0c3b2bbfbcf77eb29c1811.jpg.webp",
+      "https://images.fravega.com/f300/16e0dd1409a4055e6f4aaf0aec5cd38d.jpg.webp",
+      "https://images.fravega.com/f300/3b91cd74c4ff9df81e9737d274d577ff.jpg.webp",
+    ],
+    descripcion:
+      "La notebook Acer Aspire 3 cuenta con una pantalla de 15.6 pulgadas con resolución FHD. Está equipada con un procesador AMD Ryzen 5, capaz de manejar tareas multitarea, navegación web, reproducción de videos, y más, con gran fluidez. La Acer Aspire 3 posee una batería de 40 Wh, capaz de ofrecer hasta 11 horas de uso continuo. Viene con un almacenamiento SSD de 256GB que garantiza tiempos de carga rápidos y cuenta con 8GB de memoria SDRAM, lo que permite una multitarea fluida y un rendimiento ágil.",
+  },
 ];
